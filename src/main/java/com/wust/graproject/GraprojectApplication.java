329c7cb747f28eb7e390cd5cb4f4270cbe776349
lei <893052525@qq.com>
2019-01-03 17:57:53 +0800

    init

---
+++ src/main/java/com/wust/graproject/GraprojectApplication.java
@@ -0,0 +1,14 @@
+package com.wust.graproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraprojectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/wust/graproject/GraprojectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wust.graproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
