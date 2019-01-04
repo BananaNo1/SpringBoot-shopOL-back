c1d55b7bb3bbb44ca733a9a83b82cd6005060277
lei <893052525@qq.com>
2019-01-04 17:00:20 +0800

    全局代码 leis

--- src/main/java/com/wust/graproject/controller/LoginController.java
+++ src/main/java/com/wust/graproject/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.wust.graproject.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName LoginController
  * @Description TODO
@@ -8,7 +10,9 @@ package com.wust.graproject.controller;
  * @Version 1.0
  **/
 
+@RestController
 public class LoginController {
+
 
 
 }
